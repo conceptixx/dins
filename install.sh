057127db118f5cd6a8d0723a065832f686824cf8
Nicolas Hoeller <conceptixx@gmail.com>
2025-11-23 22:51:15 +0100

    Initial commit from local directory

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ __log() {
   local MODE="${2:-append}"
   local LOG_MSG="$(date '+%Y-%m-%d %H:%M:%S') - $1"
   case "$MODE" in
-    open) echo "$LOG_MSG" > "$LOG_FILE" ;;
-    append) echo "$LOG_MSG" >> "$LOG_FILE" ;;
+    open) sudo echo "$LOG_MSG" > "$LOG_FILE" ;;
+    append) sudo echo "$LOG_MSG" >> "$LOG_FILE" ;;
   esac
 }
 # --- __exec_mkdir()
